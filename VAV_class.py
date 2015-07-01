f4ba66a42996f8681c399a7b14afa1c276dea368
Miguel Sanchez <mglsanchez224@gmail.com>
2015-07-01 14:51:06 -0700

    added query data class and finished but not tested rogue pressures

--- VAV_class.py
+++ VAV_class.py
@@ -7,47 +7,64 @@ from ConfigParser import ConfigParser
 import json
 
 # Make these class variables into a dictionary to make it concise.
-class VAV:
-    def __init__(self, sensors):
-        self.AI_3 = sensors.get('AI_3')
-        self.AIR_VOLUME = sensors.get('AIR_VOLUME')
-        self.CLG_LOOPOUT = sensors.get('CLG_LOOPOUT')
-        self.CTL_FLOW_MAX = sensors.get('CTL_FLOW_MAX')
-        self.CTL_FLOW_MAX_PRI = sensors.get('CTL_FLOW_MAX_PRI')
-        self.CTL_FLOW_MIN = sensors.get('CTL_FLOW_MIN')
-        self.CTL_FLOW_MIN_PRI = sensors.get('CTL_FLOW_MIN_PRI')
-        self.CTL_STPT = sensors.get('CTL_STPT')
-        self.CTL_STPT_PRI = sensors.get('CTL_STPT_PRI')
-        self.DMPR_POS = sensors.get('DMPR_POS')
-        #self.HEAT.COOL = sensors.get('HEAT.COOL')
-        #self.HEAT.COOL_PRI = sensors.get('HEAT.COOL_PRI')
-        self.HTG_LOOPOUT = sensors.get('HTG_LOOPOUT')
-        self.ROOM_TEMP = sensors.get('ROOM_TEMP')
-        self.VLV_POS = sensors.get('VLV_POS')
-        self.VLV_COMD_PRI = sensors.get('VLV_COMD_PRI')
-        self.room = sensors.get('ROOM')
 
-    def find_rogue_pressure(self):
-        # gets and sets up the information. Should probably put this in a seperate private method
+
+class VAV:
+    def __init__(self, sensors, temp_control_type):
+        self.sensors = sensors
+        self.temp_control_type = temp_control_type
+
+    def _query_data(self, sensor_name, start_date, end_date, interpolation_time):
         client_obj = SmapClient("http://new.openbms.org/backend")
-        if self.DMPR_POS is None:
-            print 'no DMPR_POS info'
+        if self.sensors.get(sensor_name) is None:
+            print 'no ' + sensor_name + ' info'
             return
-        x = client_obj.query('select data in (\'4/1/2014\', \'4/8/2014\') where uuid = "' + self.DMPR_POS[0] + '"')
+        print 'select data in (\'' + start_date + '\', \'' + end_date + '\') where uuid = \'' + self.sensors.get(sensor_name)[0] + '\''
+        x = client_obj.query('select data in (\'' + start_date + '\', \'' + end_date + '\') where uuid = \'' + self.sensors.get(sensor_name)[0] + '\'')
         pos_table = pd.DataFrame(x[0]['Readings'], columns=['Time', 'Reading'])
         pos_table['Time'] = pd.to_datetime(pos_table['Time'].tolist(), unit='ms').tz_localize('UTC').tz_convert('America/Los_Angeles')
         pos_table.set_index('Time', inplace=True)
-        pos_table = pos_table.groupby(pd.TimeGrouper('5Min')).mean().interpolate(method='linear').dropna()
+        pos_table = pos_table.groupby(pd.TimeGrouper(interpolation_time)).mean().interpolate(method='linear').dropna()
+        return pos_table
 
-        # TODO- actually analyze and find the rogue pressures
+    def _find_rogue_pressure(self, date_start, date_end, interpolation_time, threshold=95):
+        if threshold is None:
+            threshold = 95
+        table = self._query_data('DMPR_POS', date_start, date_end, interpolation_time)
+        total = float(table.count())
+        count = float(table.where[table['Readings'] >= threshold].count())
+        percent = (count / total) * 100
+        return percent
+
+    def _find_rogue_temp_heat(self, date_start, date_end, interpolation_time, threshold=3):
+        if threshold is None:
+            threshold = 3
+        self._query_data(FILL, date_start, date_end, interpolation_time)
+        # TODO- actually analyze and find the rogue temp heat
+
+    def _find_rogue_temp_cool(self, date_start, date_end, interpolation_time, threshold=3):
+        self._query_data(FILL, date_start, date_end, interpolation_time)
+        # TODO- actually analyze and find the rogue temp cool
+
+    def find_rogue(self, rogue_type, threshold = None, date_start='1/1/2014', date_end='now', interpolation_time = '5Min'):
+        if rogue_type == 'pressure':
+            self._find_rogue_pressure(date_start, date_end, interpolation_time, threshold)
+        elif rogue_type == 'tempc':
+            self._find_rogue_temp_cool(date_start, date_end, interpolation_time, threshold)
+        elif rogue_type == 'temph':
+            self._find_rogue_temp_heat(date_start, date_end, interpolation_time, threshold)
+        else:
+            print rogue_type + ' is not a valid option for rogue_type'
 
 
-# key = data['S7-11']
 
 # read in the entire json, get as a dict
 with open('SDaiLimited.json') as data_file:
     data = json.load(data_file)
+#
+# for key in data.keys():
+#     inst = VAV(data[key])
+#     inst.find_rogue_pressure()
+inst = VAV(data['S2-18'], 'control')
+inst.find_rogue('pressure', date_start='4/1/2014', date_end='5/1/2014')
 
-for key in data.keys():
-    inst = VAV(data[key])
-    inst.find_rogue_pressure()
